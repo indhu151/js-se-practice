540b7c00c9f50e6b1e59be2d9d17f586f96ed48d
indhu <indhuramesh97@gmail.com>
2023-01-26 21:33:44 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,26 @@
+
+//3 types of declaration
+//1st type
+var a=10;
+//var a=20;
+console.log(a)
+ 
+//2nd  type of declaration
+let b=10;
+b=20;
+console.log(b)
+
+
+//3rd type
+const c=30;
+// c=40
+console.log(c)
+
+//to know the datatype
+var num=10;
+console.log(typeof(num))
+
+//to convert from one datatype to another
+
+console.log(typeof(String(num)))
+console.log(typeof(num))
